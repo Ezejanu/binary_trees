d9bf90fb9770935da0b62a064c4018eb33d7d82a
Ezejanu <Ezejanu_nno@ymail.com>
2023-10-04 02:28:43 -0700

    corrections to task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -21,18 +21,21 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new_node->n = value;
+	new_node->left = NULL;
+
 	if (parent->right == NULL)
 	{
 		parent->right = new_node;
 		new_node->right = NULL;
-		new_node->left = NULL;
 	}
 	else
 	{
 		new_node->right = parent->right;
-		new_node->left = NULL;
+		new_node->right->parent = new_node;
 		parent->right = new_node;
 	}
+
+	new_node->parent = parent;
 
 	return (new_node);
 }
