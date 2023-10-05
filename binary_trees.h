849fba291deca471310ba57a7532a8bd85644d38
Aishah <maishah2540@gmail.com>
2023-10-05 09:35:35 -0700

    Task 14.

--- binary_trees.h
+++ binary_trees.h
@@ -49,5 +49,4 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, 
 		const binary_tree_t *second);
 
-size_t binary_tree_heights(const binary_tree_t *tree);
 #endif
